c8010fd5f87a0c9377a9f099943042120dd58d8a
NikWhit <135679785+NikWhit@users.noreply.github.com>
2023-07-31 21:30:41 -0600

    hereAPI adds

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -13,3 +13,19 @@ async function getEventData(location) {
             localStorage.setItem("ticketmasterData", JSON.stringify(data));
         })
 }
+var platform = new H.service.Platform({
+    'apikey': hereAPI
+  });
+  // Obtain the default map types from the platform object:
+var defaultLayers = platform.createDefaultLayers();
+
+var apiURL = "https://1.base.maps.ls.hereapi.com/maptile/2.1/maptile/newest/normal.day/13/4400/2686/256/png8?apiKey={YOUR_API_KEY}"
+
+// Instantiate (and display) a map object:
+var map = new H.Map(
+    document.getElementById('mapContainer'),
+    defaultLayers.vector.normal.map,
+    {
+      zoom: 10,
+      center: { lat: 52.5, lng: 13.4 }
+    });
